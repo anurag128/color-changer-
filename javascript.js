0d3c40bd3bbf6383e1b3be3a245144c547ef1773
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-06-06 14:14:26 +0530

    added more colors

--- javascript.js
+++ javascript.js
@@ -22,5 +22,13 @@ buttons.forEach(function(button){
             body.style.backgroundColor =e.target.id;
 
         }
+        if(e.target.id === 'purple'){
+            body.style.backgroundColor =e.target.id;
+
+        }
+        if(e.target.id === 'green'){
+            body.style.backgroundColor =e.target.id;
+
+        }
     })
 });
